cc610fc7562c320b7c5198e7caf62b60accd84e3
Oswaldo Maestra <oswaldo.maestra@adib.com>
2024-04-26 09:46:36 +0400

    WIP: ArticlesListPresentationAdapter tests.

--- ArticlesConceptTests/ArticlesListPresentationAdapterTests.swift
+++ ArticlesConceptTests/ArticlesListPresentationAdapterTests.swift
@@ -6,30 +6,43 @@
 //
 
 import XCTest
+@testable import ArticlesConcept
 
 final class ArticlesListPresentationAdapterTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func test_load_displaysErrorOnLoaderError() async throws {
+        let (sut, loader) = makeSUT()
+        loader.completeLoading(with: anyNSError())
+        
+        await sut.load()
+        
+        XCTAssertEqual(sut.viewModel?.error as? NSError, anyNSError())
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    func test_load_loadsArticles() async throws {
+        let (sut, loader) = makeSUT()
+        loader.completeLoading(with: makeArticlesResult())
+        
+        await sut.load()
+        
+        XCTAssertEqual(sut.viewModel?.articles.count, 2)
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    
+    // MARK: Helpers
+    
+    private func makeSUT() -> (sut: ArticlesListPresentationAdapter, loader: ArticlesLoaderStub) {
+        let loader = ArticlesLoaderStub()
+        let viewModel = ArticlesListViewModel()
+        let sut = ArticlesListPresentationAdapter(loader: loader)
+        sut.viewModel = viewModel
+        
+        return (sut, loader)
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    private func makeArticlesResult() -> [Article] {
+        [
+            Article(title: "an article", post: "a post"),
+            Article(title: "another article", post: "another post")
+        ]
     }
-
 }
--- ArticlesConceptTests/ArticlesListUIIntegrationTests.swift
+++ ArticlesConceptTests/ArticlesListUIIntegrationTests.swift
@@ -13,32 +13,50 @@ import SwiftUI
 final class ArticlesListUIIntegrationTests: XCTestCase {
     func test_loadArticles_requestArticlesFromLoader() async {
         let (sut, loader) = makeSUT()
+        loader.completeLoading(with: makeArticlesResult())
                 
         await sut.onRefresh?()
         XCTAssertEqual(loader.loadArticlesCallCount, 2)
     }
     
-    private func makeSUT() -> (sut: ArticlesListView, loader: MockArticlesLoader) {
-        let loader = MockArticlesLoader()
+    private func makeSUT() -> (sut: ArticlesListView, loader: ArticlesLoaderStub) {
+        let loader = ArticlesLoaderStub()
         let sut = ArticlesListUIComposer.composedWith(loader: loader)
         
         return (sut, loader)
     }
-}
-
-final class MockArticlesLoader: ArticlesLoader {
-    private var requests = [Article]()
     
-    var loadArticlesCallCount: Int {
-        return requests.count
-    }
+    // MARK: Helpers
     
-    func load() async throws -> [Article] {
-        requests = [
+    private func makeArticlesResult() -> [Article] {
+        [
             Article(title: "an article", post: "a post"),
             Article(title: "another article", post: "another post")
         ]
+    }
+}
+
+final class ArticlesLoaderStub: ArticlesLoader {
+    private var articles = [Article]()
+    private var error: Error?
+    
+    var loadArticlesCallCount: Int {
+        return articles.count
+    }
+    
+    func load() async throws -> [Article] {
+        if let error {
+            throw error
+        }
         
-        return requests
+        return articles
+    }
+    
+    func completeLoading(with articles: [Article]) {
+        self.articles = articles
+    }
+    
+    func completeLoading(with error: Error) {
+        self.error = error
     }
 }
