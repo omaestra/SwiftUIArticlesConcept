246faa3673f42f0853229a821c5cd5f8c7326770
Oswaldo Maestra <oswaldo.maestra@adib.com>
2024-04-26 12:16:47 +0400

    Use Combine framework to test state updates on view model when loading from adapter.

--- ArticlesConceptTests/ArticlesListPresentationAdapterTests.swift
+++ ArticlesConceptTests/ArticlesListPresentationAdapterTests.swift
@@ -6,26 +6,70 @@
 //
 
 import XCTest
+import Combine
 @testable import ArticlesConcept
 
 final class ArticlesListPresentationAdapterTests: XCTestCase {
+    private var cancellables = Set<AnyCancellable>()
 
     func test_load_displaysErrorOnLoaderError() async throws {
+        var messages = [Message<ArticleViewModel>]()
         let (sut, loader) = makeSUT()
         loader.completeLoading(with: anyNSError())
+        sut.viewModel?.$isLoading
+            .dropFirst()
+            .sink(receiveValue: { value in
+                messages.append(.isLoading(value))
+            })
+            .store(in: &cancellables)
+        sut.viewModel?.$error
+            .dropFirst()
+            .sink(receiveValue: { error in
+                messages.append(.error(error: error as? NSError))
+            })
+            .store(in: &cancellables)
         
         await sut.load()
         
-        XCTAssertEqual(sut.viewModel?.error as? NSError, anyNSError())
+        XCTAssertEqual(messages, [.isLoading(true), .error(error: anyNSError()), .isLoading(false)])
     }
     
     func test_load_loadsArticles() async throws {
+        var messages = [Message<[ArticleViewModel]>]()
+        let articles = makeArticlesResult()
+        let mapped = makeArticlesResult().map { ArticleViewModel(title: $0.title, post: $0.post) }
         let (sut, loader) = makeSUT()
-        loader.completeLoading(with: makeArticlesResult())
+        loader.completeLoading(with: articles)
+        sut.viewModel?.$isLoading
+            .dropFirst()
+            .sink(receiveValue: { value in
+                messages.append(.isLoading(value))
+            })
+            .store(in: &cancellables)
+        sut.viewModel?.$articles
+            .dropFirst()
+            .sink(receiveValue: { articles in
+                messages.append(.loaded(resource: articles))
+            })
+            .store(in: &cancellables)
         
         await sut.load()
         
-        XCTAssertEqual(sut.viewModel?.articles.count, 2)
+        XCTAssertEqual(messages, [.isLoading(true), .loaded(resource: mapped), .isLoading(false)])
+    }
+    
+    func test_load_updatesLoadingState() async {
+        var loadingRequests: [Bool] = []
+        let (sut, _) = makeSUT()
+        
+        sut.viewModel?.$isLoading.sink(receiveValue: { value in
+            loadingRequests.append(value)
+        })
+        .store(in: &cancellables)
+        
+        await sut.load()
+        
+        XCTAssertEqual(loadingRequests, [false, true, false])
     }
     
     // MARK: Helpers
@@ -45,4 +89,10 @@ final class ArticlesListPresentationAdapterTests: XCTestCase {
             Article(title: "another article", post: "another post")
         ]
     }
+}
+
+enum Message<Resource>: Equatable where Resource: Equatable {
+    case isLoading(Bool)
+    case loaded(resource: Resource)
+    case error(error: NSError?)
 }
